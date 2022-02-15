eadc5849a8a1e3394982398942a02a06143596db
Walmsley <Eric.Walmsley@fire.ca.gov>
2022-02-15 15:05:07 -0800

    Added view and template for detialed blog post and CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views #importing all views from the blog app
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #assings a view called post_list to the root URL
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
-
